ab2f20c08f12fc0e7e2c510a86f5683ee6d2ee74
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-14 23:56:24 +0700

    add user repository

--- src/main/java/com/codegym/portfolio/model/auth/User.java
+++ src/main/java/com/codegym/portfolio/model/auth/User.java
@@ -13,7 +13,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username;
+    private String email;
 
     private String password;
 
