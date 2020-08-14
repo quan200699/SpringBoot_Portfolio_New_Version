e5e4a4e042fb2a9c3c8f99d679a40e5f30bb8fb0
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-15 00:07:29 +0700

    add jwt config

---
+++ src/main/java/com/codegym/portfolio/configuration/security/SecurityConfig.java
@@ -0,0 +1,114 @@
+package com.codegym.portfolio.configuration.security;
+
+import com.codegym.portfolio.configuration.custom.CustomAccessDeniedHandler;
+import com.codegym.portfolio.configuration.custom.RestAuthenticationEntryPoint;
+import com.codegym.portfolio.configuration.filter.JwtAuthenticationFilter;
+import com.codegym.portfolio.model.auth.Role;
+import com.codegym.portfolio.model.auth.User;
+import com.codegym.portfolio.model.enum_file.RoleName;
+import com.codegym.portfolio.service.role.RoleService;
+import com.codegym.portfolio.service.user.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private IUserService userService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    public RestAuthenticationEntryPoint restServicesEntryPoint() {
+        return new RestAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public CustomAccessDeniedHandler customAccessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @PostConstruct
+    public void init() {
+        List<User> users = (List<User>) userService.findAll();
+        List<Role> roleList = (List<Role>) roleService.findAll();
+        if (roleList.isEmpty()) {
+            Role roleAdmin = new Role();
+            roleAdmin.setId(1L);
+            roleAdmin.setName(RoleName.ADMIN.toString());
+            roleService.save(roleAdmin);
+            Role roleCoach = new Role();
+            roleCoach.setId(2L);
+            roleCoach.setName(RoleName.COACH.toString());
+            roleService.save(roleCoach);
+        }
+        if (users.isEmpty()) {
+            User admin = new User();
+            Set<Role> roles = new HashSet<>();
+            roles.add(new Role(1L, RoleName.ADMIN.toString()));
+            admin.setEmail("admin");
+            admin.setPassword(passwordEncoder.encode("123456"));
+            admin.setRoles(roles);
+            userService.save(admin);
+        }
+    }
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/**");
+        http.httpBasic().authenticationEntryPoint(restServicesEntryPoint());
+        http.authorizeRequests()
+                .antMatchers("/", "/login", "/**").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling().accessDeniedHandler(customAccessDeniedHandler());
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors();
+    }
+}
--- src/main/java/com/codegym/portfolio/model/auth/Role.java
+++ src/main/java/com/codegym/portfolio/model/auth/Role.java
@@ -1,12 +1,16 @@
 package com.codegym.portfolio.model.auth;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
-@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
