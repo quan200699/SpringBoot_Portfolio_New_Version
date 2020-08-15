a3582fabaea32cd0ba8d44fb4531426ca59dc17a
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-15 09:45:42 +0700

    add online course entity

---
+++ src/main/java/com/codegym/portfolio/model/entity/OnlineCourse.java
@@ -0,0 +1,18 @@
+package com.codegym.portfolio.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "online-courses")
+public class OnlineCourse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private boolean complete;
+}
