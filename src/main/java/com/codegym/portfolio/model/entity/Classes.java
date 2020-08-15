5b901dd6f9b1bf1a53136e0db0e16ea14ae75009
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-15 09:40:14 +0700

    add classes entity

---
+++ src/main/java/com/codegym/portfolio/model/entity/Classes.java
@@ -0,0 +1,15 @@
+package com.codegym.portfolio.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Classes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
