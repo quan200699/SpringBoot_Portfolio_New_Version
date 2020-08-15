20c7c24ea5daa636f9344b85fd756ada7d0b65d5
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-15 11:15:07 +0700

    add student service

---
+++ src/main/java/com/codegym/portfolio/service/student/IStudentService.java
@@ -0,0 +1,7 @@
+package com.codegym.portfolio.service.student;
+
+import com.codegym.portfolio.model.entity.Student;
+import com.codegym.portfolio.service.IGeneralService;
+
+public interface IStudentService extends IGeneralService<Student> {
+}
---
+++ src/main/java/com/codegym/portfolio/service/student/StudentService.java
@@ -0,0 +1,34 @@
+package com.codegym.portfolio.service.student;
+
+import com.codegym.portfolio.model.entity.Student;
+import com.codegym.portfolio.repository.IStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class StudentService implements IStudentService {
+    @Autowired
+    private IStudentRepository studentRepository;
+
+    @Override
+    public Iterable<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Optional<Student> findById(Long id) {
+        return studentRepository.findById(id);
+    }
+
+    @Override
+    public Student save(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public void remove(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
