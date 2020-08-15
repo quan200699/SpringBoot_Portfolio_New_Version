2f74e0c711e155a527e41151f2eb92e871d1a9a5
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-15 14:36:52 +0700

    add outcome service

---
+++ src/main/java/com/codegym/portfolio/service/outcome/IOutcomeService.java
@@ -0,0 +1,7 @@
+package com.codegym.portfolio.service.outcome;
+
+import com.codegym.portfolio.model.entity.Outcome;
+import com.codegym.portfolio.service.IGeneralService;
+
+public interface IOutcomeService extends IGeneralService<Outcome> {
+}
---
+++ src/main/java/com/codegym/portfolio/service/outcome/OutcomeService.java
@@ -0,0 +1,34 @@
+package com.codegym.portfolio.service.outcome;
+
+import com.codegym.portfolio.model.entity.Outcome;
+import com.codegym.portfolio.repository.IOutcomeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class OutcomeService implements IOutcomeService {
+    @Autowired
+    private IOutcomeRepository outcomeRepository;
+
+    @Override
+    public Iterable<Outcome> findAll() {
+        return outcomeRepository.findAll();
+    }
+
+    @Override
+    public Optional<Outcome> findById(Long id) {
+        return outcomeRepository.findById(id);
+    }
+
+    @Override
+    public Outcome save(Outcome outcome) {
+        return outcomeRepository.save(outcome);
+    }
+
+    @Override
+    public void remove(Long id) {
+        outcomeRepository.deleteById(id);
+    }
+}
