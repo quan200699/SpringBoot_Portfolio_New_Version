92eb09202b01efc3935551e45b65e836a1f6575b
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-15 09:46:35 +0700

    add student entity

---
+++ src/main/java/com/codegym/portfolio/model/entity/Student.java
@@ -0,0 +1,21 @@
+package com.codegym.portfolio.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String studentId;
+
+    private String name;
+
+    @ManyToOne
+    private Classes classes;
+}
