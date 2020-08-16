b02f613a1995dac6f00b4ff92a12608f7e7aab9f
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-16 09:52:05 +0700

    add product service

---
+++ src/main/java/com/codegym/portfolio/service/product/IProductService.java
@@ -0,0 +1,7 @@
+package com.codegym.portfolio.service.product;
+
+import com.codegym.portfolio.model.entity.Product;
+import com.codegym.portfolio.service.IGeneralService;
+
+public interface IProductService extends IGeneralService<Product> {
+}
