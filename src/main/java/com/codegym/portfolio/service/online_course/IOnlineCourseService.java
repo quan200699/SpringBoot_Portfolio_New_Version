c6581cda5673edb9a104cc616379a7fce77f9e5c
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-08-16 09:47:49 +0700

    add online course service

---
+++ src/main/java/com/codegym/portfolio/service/online_course/IOnlineCourseService.java
@@ -0,0 +1,7 @@
+package com.codegym.portfolio.service.online_course;
+
+import com.codegym.portfolio.model.entity.OnlineCourse;
+import com.codegym.portfolio.service.IGeneralService;
+
+public interface IOnlineCourseService extends IGeneralService<OnlineCourse> {
+}
