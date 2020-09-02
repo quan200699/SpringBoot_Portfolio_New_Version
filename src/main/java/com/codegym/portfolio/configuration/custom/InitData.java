4ff3ca05e1096000a34e8a0f77eb8d1ee94e22fe
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-02 10:26:21 +0700

    create default module done

--- src/main/java/com/codegym/portfolio/configuration/custom/InitData.java
+++ src/main/java/com/codegym/portfolio/configuration/custom/InitData.java
@@ -59,6 +59,65 @@ public class InitData {
         if (programs.isEmpty()) {
             createDefaultProgram();
         }
+        if (modules.isEmpty()) {
+            createDefaultModule();
+        }
+    }
+
+    private void createDefaultModule() {
+        Program java = new Program();
+        java.setId(1L);
+        Program php = new Program();
+        php.setId(2L);
+        Module module = new Module();
+        module.setId(1L);
+        module.setName("[BC-JAVA-BP] Module 1: Bootcamp Preparation 2.0");
+        module.setProgram(java);
+        moduleService.save(module);
+        module.setId(2L);
+        module.setName("[BC-JAVA-APJ] Module 2: Advanced Programming with Java 2.0");
+        module.setProgram(java);
+        moduleService.save(module);
+        module.setId(3L);
+        module.setName("[BC-JAVA-JWBD] Module 3: Java Web Back-end Development 2.0");
+        module.setProgram(java);
+        moduleService.save(module);
+        module.setId(4L);
+        module.setName("[BC-JAVA-WBDS] Module 4: Web Back-end Development with Spring MVC 2.0");
+        module.setProgram(java);
+        moduleService.save(module);
+        module.setId(5L);
+        module.setName("[BC-JAVA-WFDA] Module 5: Web Front-end Development Angular 2.0");
+        module.setProgram(java);
+        moduleService.save(module);
+        module.setId(6L);
+        module.setName("[BC-JAVA-PRJ] Module 6: Project and Jobs 2.0");
+        module.setProgram(java);
+        moduleService.save(module);
+        module.setId(7L);
+        module.setName("[BC-PHP-BP] Module 1: Bootcamp Preparation 2.0");
+        module.setProgram(php);
+        moduleService.save(module);
+        module.setId(8L);
+        module.setName("[BC-PHP-BP] Module 1: Bootcamp Preparation 2.0");
+        module.setProgram(php);
+        moduleService.save(module);
+        module.setId(9L);
+        module.setName("[BC-PHP-APP] Module 2: Advanced Programming with PHP 2.0");
+        module.setProgram(php);
+        moduleService.save(module);
+        module.setId(10L);
+        module.setName("[BC-PHP-WBDL] Module 3: Web Back-end Development with Laravel 2.0");
+        module.setProgram(php);
+        moduleService.save(module);
+        module.setId(11L);
+        module.setName("[BC-PHP-WFDA] Module 4: Web Front-end Development Angular 2.0");
+        module.setProgram(php);
+        moduleService.save(module);
+        module.setId(12L);
+        module.setName("[BC-PHP-WBDS] Module 5: Project and Jobs 2.0");
+        module.setProgram(php);
+        moduleService.save(module);
     }
 
     private void createDefaultProgram() {
