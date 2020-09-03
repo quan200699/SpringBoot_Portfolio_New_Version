cb83ca88291b0273368bd8388bf605d89e512071
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-03 15:02:24 +0700

    findAll class and sort

--- src/main/java/com/codegym/portfolio/repository/IClassesRepository.java
+++ src/main/java/com/codegym/portfolio/repository/IClassesRepository.java
@@ -2,8 +2,12 @@ package com.codegym.portfolio.repository;
 
 import com.codegym.portfolio.model.entity.Classes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IClassesRepository extends JpaRepository<Classes, Long> {
+    @Query(value = "select * from `portfolio-management`.classes order by id asc",
+            nativeQuery = true)
+    Iterable<Classes> findAllAndSort();
 }
